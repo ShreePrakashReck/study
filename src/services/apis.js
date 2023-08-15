e98413ce0499df47accf905cf02f9100e96c3c67
ShreePrakashReck <shreeprakash2580@gmail.com>
2023-08-15 19:48:09 +0530

    kk

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,7 +2,7 @@
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SIGNUP_API: "https://node-js-app-kappa.vercel.app/api/v1/register",
-  LOGIN_API: "https://node-js-app-kappa.vercel.app/api/v1/login",
-  FORGET_API: "https://node-js-app-kappa.vercel.app/api/v1/forgetPassword",
+  SIGNUP_API: "https://node-js-app-j1op.vercel.app/api/v1/register",
+  LOGIN_API: "https://node-js-app-j1op.vercel.app/api/v1/login",
+  FORGET_API: "https://node-js-app-j1op.vercel.app/api/v1/forgetPassword",
 };
